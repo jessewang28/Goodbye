bda6a0a914a1aa381b539ea931ded569ac524d76
student <jessewang@billericak12.com>
2019-09-13 11:18:25 -0400

    Committed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Goodbye");
+        //double x = Math.random();
+        //double x = Math.random()*10;    //random double (0.0,10.0)
+        //int x = (int) (Math.random()*10); //random integer between (0.0, 10)
+        int x = (int) (Math.random()*10) + 10; //random integer between (10.0, 20.0)
+        
+        System.out.println(x);
     }
 }
